34dbba66bc9ce57a8aeca7db8180a2927000f9fb
Ellen Arteca <ellen.arteca@gmail.com>
2021-03-09 22:47:29 -0500

    option to provide config file arg to batch runner

--- runParallelGitRepos.sh
+++ runParallelGitRepos.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 repo_link_file=$1
+config_file=$2
+
+if [ -f $config_file ]; then 
+	config_file="QL_output_config.json"
+fi
 
 # you'll probably want to bg this
-nohup parallel -j 20 -a $repo_link_file --timeout 600 --joblog job.log python diagnose_github_repo.py --repo_link {} --config QL_output_config.json 
+nohup parallel -j 20 -a $repo_link_file --timeout 600 --joblog job.log python diagnose_github_repo.py --repo_link {} --config $config_file
