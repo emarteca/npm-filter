49bb029a281411c2ff2daccc3d698e36c2f71bc0
Ellen Arteca <ellen.arteca@gmail.com>
2023-05-05 15:37:28 -0400

    update: add option for different output file for test postproc (defaults to overwriting original)

--- src/output_parsing/test_output_proc.py
+++ src/output_parsing/test_output_proc.py
@@ -5,9 +5,11 @@ import xmltodict
 # used to convert the xunit reporter output from mocha into json
 # note: this overwrites the existing file
 # code from https://www.geeksforgeeks.org/python-xml-to-json/
-def xml_to_json(output_file):
+def xml_to_json(output_file, new_output_file=None):
+    if new_output_file is None:
+        new_output_file = output_file
     with open(output_file) as xml_file:
         data_dict = xmltodict.parse(xml_file.read())
         json_data = json.dumps(data_dict)
-        with open(output_file, 'w') as json_file:
+        with open(new_output_file, 'w') as json_file:
             json_file.write(json_data)
