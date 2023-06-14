ecd7b9b10b3ee794d3eda80097b946eebf4d37ef
Jonathan Bell <jon@jonbell.net>
2023-06-14 15:56:48 +0000

    Adds CI workflow for end-to-end testing of NPM Filter

--- runDocker.sh
+++ runDocker.sh
@@ -15,6 +15,6 @@ docker run --mount type=bind,source=`pwd`/local_mount,destination=/mount \
 		   --volume `pwd`/npm_filter_docker_results:/home/npm-filter/results \
 		   --volume `pwd`/docker_configs:/home/npm-filter/docker_configs\
 		   -w /home/npm-filter \
-		   -it emarteca/npm-filter:latest \
+		   emarteca/npm-filter:latest \
 		   bash -c "PATH=/home/codeql_home/codeql:$PATH; $npm_filter_command --output_dir results"
 rm -r local_mount
