6b82cd91aecc5e4d06b220412eb1d99f4ef6bf58
Jonathan Bell <jon@jonbell.net>
2023-06-26 19:55:49 +0000

    build docker container in CI

--- runDocker.sh
+++ runDocker.sh
@@ -11,10 +11,14 @@ if [ ! -d npm_filter_docker_results ]; then
 	mkdir npm_filter_docker_results
 fi
 
+if [ -v $DOCKER_IMAGE ]; then
+	DOCKER_IMAGE=emarteca/npm-filter:latest
+fi
+
 docker run --mount type=bind,source=`pwd`/local_mount,destination=/mount \
 		   --volume `pwd`/npm_filter_docker_results:/home/npm-filter/results \
 		   --volume `pwd`/docker_configs:/home/npm-filter/docker_configs\
 		   -w /home/npm-filter \
-		   emarteca/npm-filter:latest \
+		   $DOCKER_IMAGE \
 		   bash -c "PATH=/home/codeql_home/codeql:$PATH; $npm_filter_command --output_dir results"
 rm -r local_mount
